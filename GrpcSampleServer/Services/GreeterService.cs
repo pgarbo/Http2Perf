830efe97bc0fcd19f315bfef2907784cd4467e80
Pawel Garbowski <pawel.garbowski@uk.bp.com>
2020-10-19 09:44:17 +0100

    Add payload

--- GrpcSampleServer/Services/GreeterService.cs
+++ GrpcSampleServer/Services/GreeterService.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading.Tasks;
 using Grpc.Core;
 using GrpcSample;
@@ -6,6 +7,17 @@ namespace GrpcSampleServer
 {
     public class GreeterService : Greeter.GreeterBase
     {
+        private static HelloReply _reply;
+
+        static GreeterService()
+        {
+            _reply = new HelloReply();
+            for (var i = 0; i < 1000; i++)
+            {
+                _reply.Payload.Add($"{DateTime.Now.Ticks}");
+            }
+        }
+
         public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
         {
             return Task.FromResult(new HelloReply
